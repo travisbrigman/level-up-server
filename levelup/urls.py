74c0ca860a97555c33b89c2e4e8dc80b634f3765
travisbrigman <travis1000@gmail.com>
2020-11-03 15:30:21 -0600

    chapter 9 working

--- levelup/urls.py
+++ levelup/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
 from rest_framework import routers
-from levelupapi.views import GameTypes, Games
-
+from levelupapi.views import Games, GameTypes, Events
 
 
 # urlpatterns = [
@@ -15,6 +14,7 @@ from levelupapi.views import GameTypes, Games
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypes, 'gametype')
 router.register(r'games', Games, 'game')
+router.register(r'events', Events, 'event')
 
 urlpatterns = [
     path('', include(router.urls)),
--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Event(models.Model):
     """Event database model"""
-    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
-    day = models.DateField(auto_now=False, auto_now_add=False)
+    gamer = models.ForeignKey("gamer", on_delete=models.CASCADE)
+    date = models.DateField(auto_now=False, auto_now_add=False)
     time = models.TimeField(auto_now=False, auto_now_add=False)
     location = models.CharField(max_length=75)
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
