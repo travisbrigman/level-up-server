952941006c4a16e707256e8cb349c419d5fc08c0
travisbrigman <travis1000@gmail.com>
2020-11-30 09:59:36 -0600

    got html templating working

---
+++ levelupreports/views/connection.py
@@ -0,0 +1,2 @@
+class Connection:
+    db_path = "/Users/travislaptop/workspace/python/levelup/db.sqlite3"
---
+++ tests/event_tests.py
@@ -0,0 +1,32 @@
+import json
+from rest_framework import status
+from rest_framework.test import APITestCase
+from levelupapi.models import event
+
+class EventTests(APITestCase):
+    def setUp(self):
+        """
+        Create a new account and create sample category
+        """
+        url = "/register"
+        data = {
+            "username": "steve",
+            "password": "Admin8*",
+            "email": "steve@stevebrownlee.com",
+            "address": "100 Infinity Way",
+            "phone_number": "555-1212",
+            "first_name": "Steve",
+            "last_name": "Brownlee",
+            "bio": "Love those gamez!!"
+        }
+        # Initiate request and capture response
+        response = self.client.post(url, data, format='json')
+
+        # Parse the JSON in the response body
+        json_response = json.loads(response.content)
+
+        # Store the auth token
+        self.token = json_response["token"]
+
+        # Assert that a user was created
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
